99826cc110398bc487c37219bf741fd4eb6f8eb6
Greg Lanthier <greg.lanthier@ca.ibm.com>
2019-03-12 21:45:57 -0400

    Typo.

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // Package gocflapi contains a client for the CFL API (http://api.cfl.ca/docs).
 //
-// The code was generated using the https://github.com/go-swagger/go-swagger project and a hand-written Swagger 2.0 spec file (https://github.com/greglanthier/gocflapi/blob/master/api/cflapi.yaml).
+// The code was generated using the https://github.com/go-swagger/go-swagger project from a hand-written Swagger 2.0 spec file (https://github.com/greglanthier/gocflapi/blob/master/api/cflapi.yaml).
 //
 package gocflapi
